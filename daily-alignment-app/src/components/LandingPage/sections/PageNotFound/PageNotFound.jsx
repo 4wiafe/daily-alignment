e5f4428093d2868efa4acab91668474605503994
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-13 11:06:09 +0000

    Style page not found component

--- daily-alignment-app/src/components/LandingPage/sections/PageNotFound/PageNotFound.jsx
+++ daily-alignment-app/src/components/LandingPage/sections/PageNotFound/PageNotFound.jsx
@@ -7,7 +7,7 @@ export default function PageNotFound() {
       <h1>404 Error</h1>
       <h2>Page not found</h2>
       <p>The page you are looking for does not exist.</p>
-      <Link to="/">Go to the Home Page</Link>
+      <Link to="/" className="home-page-link">Go to the Home Page</Link>
     </div>
   );
 }
