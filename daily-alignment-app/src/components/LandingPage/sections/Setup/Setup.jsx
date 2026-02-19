fbfa235576836b664bcdb3272a54b3df44874e07
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-19 15:43:21 +0000

    Style the setup section

--- daily-alignment-app/src/components/LandingPage/sections/Setup/Setup.jsx
+++ daily-alignment-app/src/components/LandingPage/sections/Setup/Setup.jsx
@@ -2,22 +2,24 @@ import "./Setup.css";
 
 export default function Setup() {
   return (
-    <section>
-      <div className="per-day">
-        <p className="time">60</p>
-        <p className="time-des">seconds per day</p>
-      </div>
-      <div className="price">
-        <p className="rate">100%</p>
-        <p className="rate-des">Free</p>
-      </div>
-      <div className="hassle">
-        <p className="hassle-num">0</p>
-        <p className="hassle-des">Setup hassle</p>
-      </div>
-      <div className="potential">
-        <p className="infinite">∞</p>
-        <p className="potential-des">seconds per day</p>
+    <section id="setup">
+      <div className="setup-container">
+        <div className="per-day">
+          <p className="green">60</p>
+          <p className="white">seconds per day</p>
+        </div>
+        <div className="price">
+          <p className="green">100%</p>
+          <p className="white">Free</p>
+        </div>
+        <div className="hassle">
+          <p className="green">0</p>
+          <p className="white">Setup hassle</p>
+        </div>
+        <div className="potential">
+          <p className="green">∞</p>
+          <p className="white">Potential unlocked</p>
+        </div>
       </div>
     </section>
   );
