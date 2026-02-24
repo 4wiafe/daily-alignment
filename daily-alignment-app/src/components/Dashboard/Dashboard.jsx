8a183335a45e291b74e037c506d75d25aecdde5d
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-02-24 11:38:29 +0000

    Refactor dashboard component

--- daily-alignment-app/src/components/Dashboard/Dashboard.jsx
+++ daily-alignment-app/src/components/Dashboard/Dashboard.jsx
@@ -1,83 +1,33 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import GoalSection from "./GoalSelection";
 import "./Dashboard.css";
 
-const GOAL_KEY = "goal";
-
-function loadGoal() {
-  try {
-    const saved = localStorage.getItem(GOAL_KEY);
-    const parsed = JSON.parse(saved);
-
-    if (!parsed) return "";
-
-    return parsed;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
- export default function Dashboard() {
-  const [goal, setGoal] = useState("");
-  const [savedGoal, setSavedGoal] = useState(loadGoal());
-
-  const handleGoal = (event) => {
-    setGoal(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    setSavedGoal(goal);
-    setGoal("");
-
-    localStorage.setItem(KEY, JSON.stringify(goal));
-  };
-
-  return(
+export default function Dashboard() {
+  return (
     <>
       <header className="dashboard-header">
         <nav className="dashboard-nav">
           <div className="dashboard logo-container">
-            <Link to="/dashboard" className="dashboard logo">Daily Alignment</Link>
+            <Link to="/dashboard" className="dashboard logo">
+              Daily Alignment
+            </Link>
             <p>Build momentum, achieve goals</p>
           </div>
+
           <div className="dashboard-links">
-            <Link to="/dashboard" className="dashboard-link">Dashboard</Link>
-            <Link to="/checkIn" className="check-in-link">Daily Check-In</Link>
+            <Link to="/dashboard" className="dashboard-link">
+              Dashboard
+            </Link>
+            <Link to="/checkIn" className="check-in-link">
+              Daily Check-In
+            </Link>
           </div>
         </nav>
       </header>
 
       <main className="dashboard-main">
-        <section id="goal">
-          <div className="goal-form">
-            {
-            savedGoal ? (
-              <p className="set-goal">{savedGoal}</p>
-            ) : (
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="goal" className="goal-label">
-                  Set Your Goal
-                </label>
-                <input 
-                    type="text" 
-                    name="goal" 
-                    id="goal"  
-                    className="goal-input"
-                    value={goal}
-                    onChange={handleGoal}
-                    placeholder="What is your main goal?"
-                    required
-                  />
-                <button 
-                  type="submit" className="submit">Set goal</button>
-              </form>
-            )
-          }
-          </div>
-        </section>
+        <GoalSection />
       </main>
     </>
   );
- }
+}
